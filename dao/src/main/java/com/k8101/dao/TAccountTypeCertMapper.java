b5c598464ae63b86b809484131ca5eed07db5c6e
zhaomengyang <654919432@qq.com>
2019-10-08 21:33:12 +0800

    第二次提交

--- dao/src/main/java/com/k8101/dao/TAccountTypeCertMapper.java
+++ dao/src/main/java/com/k8101/dao/TAccountTypeCertMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 
 public interface TAccountTypeCertMapper {
     long countByExample(TAccountTypeCertExample example);
-
+//张三
     int deleteByExample(TAccountTypeCertExample example);
 
     int deleteByPrimaryKey(Integer id);
